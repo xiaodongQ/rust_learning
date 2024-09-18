80aeaa335c1cddab6f02fe49fc6c28a34ea7c2a9
xiaodongQ <xiaodong0795@163.com>
2024-09-19 00:12:22 +0800

    example

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -14,6 +14,25 @@ fn main() {
     test_loop();
     println!("=============");
     test_for();
+    println!("=============");
+    my_condition();
+
+    println!("=============");
+    test_while();
+}
+
+fn my_condition() {
+    let n = 6;
+
+    if n % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if n % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if n % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
 
 fn my_function(x : i32) {
@@ -28,15 +47,37 @@ fn test_loop(){
     let mut i = 0;
     loop{
         if i>4{
+            // 加不加分号都可以，因为loop是一个表达式，可以返回值
             break
         }
         i = i+1;
         println!("again!");
     }
+    println!("test_loop end!")
 }
 
 fn test_for(){
+    // rev()表示逆序，此处即3、2、1
     for num in (1..4).rev(){
         println!("{}!", num);
     }
+
+    // 可用`_`忽略变量消除未使用警告
+    for _ in 1..4 {
+        println!("again!");
+    }
+
+    println!("test_for end!")
+}
+
+fn test_while() {
+    let mut n = 0;
+
+    while n <= 5  {
+        println!("{}!", n);
+
+        n = n + 1;
+    }
+
+    println!("test_while end!")
 }
