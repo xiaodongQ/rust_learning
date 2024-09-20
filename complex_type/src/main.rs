71023179276dc1686797db9d5a3f972d00f384ce
xiaodongQ <xiaodong0795@163.com>
2024-09-21 07:11:11 +0800

    test

--- complex_type/src/main.rs
+++ complex_type/src/main.rs
@@ -7,6 +7,12 @@ struct User {
     email: String,
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn test_struct(){
     // 下面需要修改user，则此处需要定义为 mut
     let mut user = User {
@@ -18,4 +24,10 @@ fn test_struct(){
     // 修改user的值
     user.username = String::from("xdtest2");
     println!("username: {}, email: {}", user.username, user.email);
+
+    // 借助 `#[derive(Debug)]` 和 {:?} 打印结构体信息
+    let rect = Rectangle { width: 30, height: 50,};
+    println!("{:?}", rect);
+    // {:#?} 会在打印时带上缩进
+    println!("{:#?}", rect);
 }
