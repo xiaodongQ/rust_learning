2ec4104ed91b17cabefaa7ba6026b2c93daa0eae
xiaodongQ <xiaodong0795@163.com>
2024-09-26 07:34:28 +0800

    优化

--- minigrep/src/bin/main4.rs
+++ minigrep/src/bin/main4.rs
@@ -1,5 +1,8 @@
 /// 优化1：解析传入参数抽取为函数
 /// 进一步优化2：解析传入参数抽取为函数，并返回结构体
+/// 进一步优化3：
+///     面向对象编程，Config实例通过对象的方法来返回，而不是通过函数返回值
+///     参数解析的错误处理，通过panic!宏来处理
 /// 
 use std::env;
 use std::fs;
@@ -8,7 +11,8 @@ fn main() {
     // 模块化代码
     // 通过 env::args() 获取命令行参数，返回一个迭代器。而后用 collect 方法输出一个集合类型 Vector
     let args : Vec<String> = env::args().collect();
-    let config = parse_args(&args);
+    // 相应地，此处从函数调整为使用结构体的方法来创建实例
+    let config = Config::new(&args);
     println!("cmd:{}, query:{}, file_path:{}", &args[0], config.query, config.file_path);
 
     // 通过std::fs模块的 read_to_string 读取文件内容
@@ -25,12 +29,17 @@ struct Config {
     file_path : String,
 }
 
-// 传入不可变引用
-// 返回切片元组，2个元素，第一个为查询字符串，第二个为文件名
-fn parse_args(args : &Vec<String>) -> Config {
-    // 这里新创建一个String，拥有单独的所有权
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+// 通过 impl 关键字来实现方法
+impl Config {
+    // 动态数组的不可变引用，可以用数组切片来代替
+    // fn new(args : &Vec<String>) -> Config {
+    fn new(args : &[String]) -> Config {
+        if args.len() != 3 {
+            panic!("args len:{} not valid!", args.len());
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config{query, file_path}
+        Config{query, file_path}
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,10 @@
-/// 优化1：解析传入参数抽离为函数
-/// 进一步优化2：解析传入参数抽离为函数，并返回结构体
+/// 优化1：解析传入参数抽取为函数
+/// 进一步优化2：解析传入参数抽取为函数，并返回结构体
 /// 进一步优化3：
 ///     面向对象编程，Config实例通过对象的方法来返回，而不是通过函数返回值
-///     并优化参数解析的错误码
+///     参数解析的错误处理，通过panic!宏来处理
+/// 进一步优化4：使用Result<T, E>来处理错误
+///    
 /// 
 use std::env;
 use std::fs;
@@ -11,8 +13,12 @@ fn main() {
     // 模块化代码
     // 通过 env::args() 获取命令行参数，返回一个迭代器。而后用 collect 方法输出一个集合类型 Vector
     let args : Vec<String> = env::args().collect();
-    // 相应地，此处从函数调整为使用结构体的方法来创建实例
-    let config = Config::new(&args);
+    // 此处 unwrap_or_else 是Result实现的方法，使用闭包来处理错误
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        // 标准库，处理进程退出
+        std::process::exit(1);
+    });
     println!("cmd:{}, query:{}, file_path:{}", &args[0], config.query, config.file_path);
 
     // 通过std::fs模块的 read_to_string 读取文件内容
@@ -31,15 +37,16 @@ struct Config {
 
 // 通过 impl 关键字来实现方法
 impl Config {
-    // 动态数组的不可变引用，可以用数组切片来代替
-    // fn new(args : &Vec<String>) -> Config {
-    fn new(args : &[String]) -> Config {
+    // 从代码惯例的角度出发，new 往往不会失败，修改函数名为 build
+    // 传入参数：动态数组的不可变引用，可以用数组切片来代替
+    // 通过Result<T, E>返回错误， 指定 &str 的生命周期为 'static
+    fn build(args : &[String]) -> Result<Config, &'static str> {
         if args.len() != 3 {
-            panic!("args len:{} not valid!", args.len());
+            return Err("args length invalid");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config{query, file_path}
+        return Ok(Config { query, file_path });
     }
 }
